c353be54cf13814d637493ad18f2f3ad0c427248
abs-rio09 <abubakkar07.cse@gmail.com>
2026-04-15 03:53:23 +0600

    Initial commit

---
+++ divisible2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    // int n;
+    // scanf("%d",&n);
+    // for(int i=1; i<=n; i++)
+    // {
+    //     if(i%3 == 0 && i%7 == 0){
+    //         printf("%d \n",i);
+    //     }
+    // }
+
+    char ch1 = '/';
+    char ch2 ='\\';
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. %c %c %c %c %c %c\n",ch1,ch2,ch1,ch2,ch1,ch2);
+    printf("I want to give my 100%% dedication to learn! \tI will succeed one day.\n");
+}
---
+++ do_while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=3;
+    do{
+        printf("%d \n",i);
+        i++;
+    }
+    while(i<=15);
+
+    return 0;
+}
