5dbc01d39a2adb13fe6e3655e2e7bf1a91796593
abs-rio09 <abubakkar07.cse@gmail.com>
2026-04-24 02:01:20 +0600

    Array

--- Array/printing_reverse_of_an_array.c
+++ Array/printing_reverse_of_an_array.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
